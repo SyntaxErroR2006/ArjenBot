754bf2dca8bfc735642674fe920f036bf9c51ad8
SyntaxErroR2006 <70145134+SyntaxErroR2006@users.noreply.github.com>
2020-09-22 18:29:06 +0200

    Update info.js

--- commands/info.js
+++ commands/info.js
@@ -4,7 +4,7 @@ module.exports.run = async(client, message, args) => {
 
     try{
 
-        var text = "**ArjenBot** \n\n **__Commands__** \n -info - Geeft dit scherm. \n -new - Maakt een ticket. \n -serverinfo - Geeft info over de server. \n -leden - Geeft weer hoeveel leden/bots er zijn. \n -ping - Geeft weer hoeveel ping je hebt. \n -game - Geeft de game link. \n\n  **__Staff Commands__** \n -training - Hiermee kan je een training inplannen. \n -ban - Hiermee kan je iemand bannen.\n \n -kick - Kickt een speler. \n -close - Closed een ticket. \n -claim - Je claimed een ticket. \n -clear - Je verwijderd tekst \n -giveaway je start een giveaway \n µ"
+        var text = "**ArjenBot** \n\n **__Commands__** \n -info - Geeft dit scherm. \n -new - Maakt een ticket. \n -serverinfo - Geeft info over de server. \n -leden - Geeft weer hoeveel leden/bots er zijn. \n -ping - Geeft weer hoeveel ping je hebt. \n -game - Geeft de game link. \n\n  **__Staff Commands__** \n -training - Hiermee kan je een training inplannen. \n -ban - Hiermee kan je iemand bannen.\n -kick - Kickt een speler. \n -close - Closed een ticket. \n -claim - Je claimed een ticket. \n -clear - Je verwijderd tekst \n -giveaway je start een giveaway."
         
         message.author.send(text);
 
