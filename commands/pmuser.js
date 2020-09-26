4564439faaa1c8894c3bb0ed8716bcf755ca7e87
SyntaxErroR2006 <70145134+SyntaxErroR2006@users.noreply.github.com>
2020-09-26 18:20:42 +0200

    Create pmuser.js

---
+++ commands/pmuser.js
@@ -0,0 +1,27 @@
+const discord = require("discord.js");  
+
+module.exports.run = async(client, message, args) => {
+
+    if(!args[0]) return message.channel.send("Geen gebruiker opgegeven.");
+
+    var member = message.guild.member(message.mentions.users.first() || client.users.cache.get(args[0]));
+    if(!member) return message.channel.send("Gebruiker niet gevonden.");
+
+    var text = args.join(" ").slice(args[0].lenght + 1);
+    if(!text) return message.channel.send("Geen bericht opgegeven.");
+
+    message.member.seend(text).then(() => {
+        message.channel.send("Bericht is verzonden!");
+    }).catch(() => {
+        message.channel.send(":x: De persoon heeft zijn privé berichten uit staan.");
+    });
+
+
+
+    
+
+}
+
+module.exports.help = {
+    name: "pm"
+}
