7aa1404ffd9b97591ea3b8c085241069d2433b2b
SyntaxErroR2006 <70145134+SyntaxErroR2006@users.noreply.github.com>
2020-09-26 17:40:47 +0200

    z

---
+++ commands/userinfo.js
@@ -0,0 +1,35 @@
+const discord = require("discord.js");
+const moment = require("moment");
+
+module.exports.run = async (client, message, args) => {
+
+    var member = message.guild.member(message.mentions.users.first() || client.users.cache.get(args[0]));
+    if (!member) member = message.member;
+
+    var roles = member.roles.cache.size - 1;
+    var roleNames = member.roles.cache.map(r => r).join(" ").replace("@everyone", "");
+    if (roles == 0) roleNames = "Geen rollen";
+
+    var status = member.presence.status;
+
+    var nickName = member.nickname;
+    if (nickName == null || undefined) nickName = "Geen";
+
+    var embed = new discord.MessageEmbed()
+        .setColor("")
+        .setThumbnail(member.user.displayAvatarURl({ size: 4096 }))
+        .setTitle(`${member.user.tag}`)
+        .addField("ID:", `${member.id}`, true)
+        .addField("Bijnamen:", nickName, true)
+        .addField("Status:", `${status}`, true)
+        .addField("Game:", `${member.presence.activities[0] ? member.presence.activities[0].name : 'Geen'}`, true)
+        .addField("Account gemaakt", `${moment(member.user.createdAt).format("LL")}`)
+        .addField("Server gejoined:", `${moment(member.joinedAt).format('LL')}`)
+        .addField(`Rollen [${roles}]`, `${roleNames}`);
+    
+    message.channel.send(embed);    
+}
+
+module.exports.help = {
+    name: "userinfo"
+}
